2e2c7384661a6ab4f13e0eb4a8666ad0a5fa4d62
Makar <vsyakofazno@gmail.com>
2023-02-15 21:41:24 +0300

    Задачи на конструкции if-else, switch-case в JavaScript

---
+++ src/js/test.js
@@ -0,0 +1,40 @@
+
+
+
+let booleanValue = false;
+if (booleanValue == true){
+    console.log("Верно")
+} 
+else{
+    console.log("Неверно")
+}
+let booleanValue1 = false;
+let result = (booleanValue1 != true) ? true: false ;
+console.log(result);
+
+
+let a = 2;
+if (a > 0 || a < 5){
+    console.log("Верно")
+}else{
+    console.log("Неверно")
+}
+
+let valueA = 7;
+let valueB = 2;
+let c;
+if(valueA <= 1 || valueB >= 3 ){
+    console.log(valueA+valueB);
+}else{
+    console.log(valueA-valueB);
+}
+
+
+let A = 14;
+let B = 2;
+
+if(A > 2 && A<11 || B >= 6 && B <14){
+    console.log("true");
+}else{
+    console.log("false");
+}
