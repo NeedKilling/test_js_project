822217fadce7632136574322cca26b3f1020d221
Makar <vsyakofazno@gmail.com>
2023-02-20 18:47:24 +0300

    pyramid for star

--- src/js/pyramid.js
+++ src/js/pyramid.js
@@ -1,7 +1,7 @@
 const lines = 5;
     let res = '';
     for(let i = 0;i<=lines;i++){  // создание строк
-        for(let j = 0;j < lines-i;j++){  // создание пробелов
+        for(let j = 0;j <=lines-i;j++){  // создание пробелов
             res+=" "
         }
         for (let j = 0; j < (2 * i + 1); j++) { // создание звезд
