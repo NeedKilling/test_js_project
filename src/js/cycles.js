283775d4ed3245b9586c356f12fa32fad6212a6f
Makar <vsyakofazno@gmail.com>
2023-02-20 18:34:25 +0300

    tasks of cycles

--- src/js/cycles.js
+++ src/js/cycles.js
@@ -86,6 +86,8 @@ for(let i = 5; i <= 10;i++){
 
 return arrayOfNumbers;
 
+
+
 const arra = [];
 
 for(let i = 4; i <= 10;i++){
@@ -96,3 +98,56 @@ for(let i = 4; i <= 10;i++){
 return arra;
 
 
+const ark = [3,5,8,16,20,23,50];
+const resultat = [];
+
+for(let i = 1; i <= ark.length;i++){
+    resultat[i-1] = ark[i-1];
+}
+
+console.log(resultat)
+console.log(ark)
+
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+for (let i = 0; i < data.length; i++) {
+    if(typeof(data[i]) === 'number'){
+       data[i] *= 2;
+    }else if(typeof(data[i]) === 'string'){
+        data[i] = `${data[i]} - done`
+        // data[i] + ' - done'
+    }
+}
+console.log(data);
+
+let vr = 'чумбик';
+if(typeof(vr) === 'number'){
+    console.log("это число")
+}else if(typeof(vr) === 'string'){
+    console.log("а это строка")
+}
+
+
+const datar = [5, 10, 'Shopping', 20, 'Homework'];
+    const resulttut = [];
+    for(let i = 1; i<=datar.length;i++){
+        resulttut[i-1] = datar[datar.length-i]
+    }
+    console.log(resulttut)
+
+
+
+
+    const lines = 7;
+    let res = '';
+    for(let i = 0;i<lines;i++){
+        for(let j = 0;j < i;j++){
+            for(let k = lines;k > j;k--){
+                res+=' '
+            }
+            res +="*"
+        }
+        res +='\n'
+    }
+console.log(res)
+
+   
