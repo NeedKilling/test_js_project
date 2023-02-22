c8dd684bf99becc66fdc14ff3c6eb9d8507e11c2
Makar <vsyakofazno@gmail.com>
2023-02-22 19:12:05 +0300

    tasks

--- src/js/script.js
+++ src/js/script.js
@@ -1,23 +1,5 @@
 "use strict"
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?","");
-
-let PersonalMovieDB = {
-    count:numberOfFilms,
-    movies:{},
-    actors:{},
-    genres:[],
-    privat:false
-}
-const a = prompt("Какой последний фильм вы посмотрели?",""),
-    b = prompt("На сколько вы его оцените по десяти бальной шкале",""),
-    c = prompt("Какой последний фильм вы посмотрели?",""),
-    d = prompt("На сколько вы его оцените по десяти бальной шкале","");
-
-    PersonalMovieDB.movies[a] = b;
-    PersonalMovieDB.movies[c] = d;
-console.log(PersonalMovieDB);
-
 
 let result;
 let num  = 4;
@@ -41,4 +23,67 @@ switch(num){
 }
 
 
+
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?","");
+
+let PersonalMovieDB = {
+    count:numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat:false
+}
+
+    //PersonalMovieDB.movies[a] = b;
+
+//   for(let i = 0;i<2;i++){
+//         const a = prompt("Какой последний фильм вы посмотрели?",""),
+//               b = prompt("На сколько вы его оцените по десяти бальной шкале","");
+//         if(a !=null && b !=null && a != '' && b != '' && a.length < 50 && b.length < 50 ){
+//             PersonalMovieDB.movies[a] = b;
+//             console.log("done"); 
+//         }else{
+//             console.log("error")
+//             i--;
+//         }
+//     }
+    // let r = 0;
+    // while(r<2){
+    //     const a = prompt("Какой последний фильм вы посмотрели?",""),
+    //           b = prompt("На сколько вы его оцените по десяти бальной шкале","");
+    //     if(a !=null && b !=null && a != '' && b != '' && a.length < 50 && b.length < 50 ){
+    //         PersonalMovieDB.movies[a] = b;
+    //         console.log("done"); 
+    //     }else{
+    //         console.log("error")
+    //         r--;
+    //     }
+    //     r++;
+    // }
+    let r = 0;
+    do{
+        const a = prompt("Какой последний фильм вы посмотрели?",""),
+        b = prompt("На сколько вы его оцените по десяти бальной шкале","");
+            if(a !=null && b !=null && a != '' && b != '' && a.length < 50 && b.length < 50 ){
+                PersonalMovieDB.movies[a] = b;
+                console.log("done"); 
+            }else{
+                console.log("error")
+                r--;
+            }
+        r++;
+    }while(r<2)
+
+
+    if(PersonalMovieDB.count < 10){
+        alert("Довольно мало фильмов вы посмотрели.")
+    }else if (PersonalMovieDB.count >= 10 && PersonalMovieDB <= 30) {
+        alert("Вы классический зритель.")
+    }else if(PersonalMovieDB.count>30){
+        alert("ВЫ гуру фильмов")
+    }else{
+        alert("Произошла ошибка")
+    }
+
+console.log(PersonalMovieDB);
 
